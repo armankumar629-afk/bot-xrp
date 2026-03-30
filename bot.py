8563a1a89e5ed63aa811948b1f749eb017785283
armankumar629-afk <armankumar629@gmail.com>
2026-03-30 23:37:09 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,8 +32,8 @@ WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET", "blackcat2024")
 
 SYMBOL = "XRPUSDT"
 PRODUCT_TYPE = "USDT-FUTURES"
-LEVERAGE = 13
-RISK_PERCENT = 95  # % of balance per trade
+LEVERAGE = 15
+RISK_PERCENT = 96  # % of balance per trade
 MARGIN_MODE = "crossed"
 
 BITGET_BASE_URL = "https://api.bitget.com"
